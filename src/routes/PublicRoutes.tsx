5c83826437ec9f759f3eb3d2d83e0472f9d88783
juan josé martínez pantoja <pantojam48@gmail.com>
2025-03-12 23:57:24 -0600

    feat: Se modifican rutas de PublicRoutes

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Navigate, Route, Router, Routes } from "react-router-dom";
-import App from "../App";
-import { HomeView } from "../Views/HomeView";
-import { MoviesView } from "../Views/MoviesView";
+import AuthRoutes from "./AuthRoutes";
 
 export const PublicRoutes = () => {
   return (
@@ -12,7 +10,6 @@ export const PublicRoutes = () => {
         <Route path="**" element={<Navigate to="/auth/login" />} />
         <Route path="/*" element={<Navigate to="/auth/login" />} />
         <Route path="/auth/*" element={<AuthRoutes />} />
-        <Route path="/private/*" element={<PortalNegocio />}></Route>
       </Routes>
       {/* <Route path="/" Component={MoviesView} /> */}
     </Routes>
